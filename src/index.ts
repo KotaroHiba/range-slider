d63e47a04c6be6236e771607046f28192fa8549f
SWDevStudio <62483809+SWDevStudio@users.noreply.github.com>
2020-04-11 18:51:14 +0300

    11

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import './css/range-slider.scss'
-
+// import './css/range-slider.scss'
+import './css/orange.sass'
 
 import Presenter from "./module/Presenter";
 let test = new Presenter();
@@ -8,12 +8,17 @@ function createLoadingBar(value : number) {
     let stepValue : number = value / 4;
     let stepOne = value / 4;
     let step : number[] = [0];
-    for (let i =0; i < 4; i++) {
+    let block = document.getElementById('test');
+    step.unshift(0);
+    for (let i = 1; i < 6; i++) {
         step[i]= stepValue;
+        block.innerHTML = block.innerHTML + `<div>${step[i-1]}</div>`
         stepValue += stepOne;
     }
-    return step;
+
 }
 
 
 console.log(createLoadingBar(100));
+
+
